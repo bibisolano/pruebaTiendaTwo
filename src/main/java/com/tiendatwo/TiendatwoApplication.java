01bbec46fb92ed531a5ce2ab4da20be396481fbe
milu <milu@DESKTOP-D3BLQOR>
2026-05-31 14:56:04 -0600

    commit: 0 PRUEBA 3 INICIALIZACION

---
+++ src/main/java/com/tiendatwo/TiendatwoApplication.java
@@ -0,0 +1,13 @@
+package com.tiendatwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendatwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendatwoApplication.class, args);
+	}
+
+}
